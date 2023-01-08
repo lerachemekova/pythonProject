2ba232d5bb021f0cd55f074f4a10092f79ae5fa1
lerachemekova <lera.chemekova@mail.ru>
2023-01-08 12:46:02 +0300

    for

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-a = int(input())
-b = input()
-c = int(input())
-if b == '+':
-    print(a+c)
-if b == '-':
-    print(a-c)
-if b == '*':
-    print(a*c)
-if b == '/':
-    if c == 0 :
-        print("на 0 делить нельзя")
-    else:
-        print(a/c)
+for i in range(1, 10):
+    print(i)
