2c458d980ad84edc92e931a15f1dfb6d3b1efa02
lerachemekova <lera.chemekova@mail.ru>
2023-01-08 13:30:01 +0300

    6

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-a =int(input())
-if a in [12, 1, 2]:
-    print("зима")
-if a in [3, 4, 5]:
-    print("весна")
-if a in [6, 7, 8]:
-    print("лето")
-if a in [9, 10, 11]:
-    print("осень")
+x =int(input())
+y =int(input())
+if x>0 and y>0:
+     print(x+y)
+if x<0 and y<0:
+     print(x+y)
+if x>0 and y<0:
+     print((x+y)/2)
+if x<0 and y>0:
+     print((x+y)/2)
+
+
