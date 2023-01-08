c7c3499ab6622f9f5695363be11f6291a0b9d5ea
lerachemekova <lera.chemekova@mail.ru>
2023-01-08 13:18:19 +0300

    4

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 a =int(input())
-if a>0:
-    print(a+1)
-if a<0:
-    print(a-2)
+c =int(input())
+if c == 1:
+    print(a)
+if c == 2:
+    print(a*1000000)
+if c == 3:
+    print(a*1000)
+if c == 4:
+    print(a/1000)
+if c == 5:
+    print(a/100)
