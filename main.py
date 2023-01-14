26e6b21f6549967e2c8adcdc0698786c2532207e
lerachemekova <lera.chemekova@mail.ru>
2023-01-14 23:09:13 +0300

    доделать

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import random
-
-a =random.randint(1, 7)
-if a ==3:
-    print('ты проиграл')
-else:
-    print('ты выиграл')
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+n = a + d
+m = b - c
+if n == m:
+    print("верно")
+if n < m:
+    print("не верно")
+if n > m:
+    print("не верно")
